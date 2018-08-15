adfb603b9b81734c0b357903ee35921fb8c1a7e1
Nhat Vo <vothanhnhat85@gmail.com>
2018-08-15 20:42:07 -0300

    fix gcc-linaro-7.3.1-2018.05

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -101,7 +101,9 @@ mksysmap()
 
 sortextable()
 {
-	${objtree}/scripts/sortextable ${1}
+	cp ${1} /tmp/.${1}
+    scripts/sortextable /tmp/.${1}
+    cp /tmp/.${1} ${1}
 }
 
 # Delete output files in case of error
